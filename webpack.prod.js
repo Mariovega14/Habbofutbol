9ca7363bcb3f42540a9a66ee6367339838fecf65
habbofutbol <futbolenhabbo@gmail.com>
2025-03-21 22:38:36 -0500

    desde local

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
@@ -8,8 +9,9 @@ module.exports = merge(common, {
     },
     plugins: [
         new Dotenv({
-            safe: true,
-            systemvars: true
+            systemvars: true,        // Usa variables del entorno del sistema (como en Vercel)
+            allowEmptyValues: true,  // No falla si una variable está vacía
+            path: null               // No busca un .env local (opcional pero explícito)
         })
     ]
 });
